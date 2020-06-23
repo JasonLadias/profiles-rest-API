660ed214da45ace97a0728478c43f6ad9dd427ab
Jason <jason.ladias@gmail.com>
2020-06-23 17:18:49 +0300

    migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'profiles_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'profiles',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost'
     }
 }
 
@@ -121,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
